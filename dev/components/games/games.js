16660cc2909a8ddb1045d4466c10e0cd7b9b610d
Nikita Mihalyov <nikita.mihalyov@gmail.com>
2021-01-21 10:50:14 +0300

    Games page adaptive

--- dev/components/games/games.js
+++ dev/components/games/games.js
@@ -4,5 +4,13 @@ $('.js-games-slider').slick({
   slidesToShow: 6,
   variableWidth: true,
   prevArrow: '<div class="games__slider-arrow games__slider-arrow--prev"></div>',
-  nextArrow: '<div class="games__slider-arrow games__slider-arrow--next"></div>'
+  nextArrow: '<div class="games__slider-arrow games__slider-arrow--next"></div>',
+  responsive: [{
+    breakpoint: 1199,
+    settings: {
+      swipe: true,
+      swipeToSlide: true,
+      slidesToShow: 3
+    }
+  }]
 });
